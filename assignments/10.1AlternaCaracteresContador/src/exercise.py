0fd27a48986ca35d6747f9e55492a1ccc987597f
A01662767 <89958847+A01662767@users.noreply.github.com>
2021-09-07 12:51:52 -0500

    Update exercise.py

--- assignments/10.1AlternaCaracteresContador/src/exercise.py
+++ assignments/10.1AlternaCaracteresContador/src/exercise.py
@@ -1,6 +1,15 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    print("NUMERO CON DIJITOS")
+    n = int(input("CANTIDAD DE NUMEROS: "))
+ 
+    for i in range(n):
 
-if __name__=='__main__':   
+        if i %2==0:
+            print(i, "-%")
+        else:
+            print(i, "-#")
+
+    pass
+if __name__=='__main__':
     main()
