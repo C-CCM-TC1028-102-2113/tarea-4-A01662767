e81a77bec7c8cc7bdbbac87e05bf55c227ad0ce6
A01662767 <89958847+A01662767@users.noreply.github.com>
2021-09-07 12:26:14 -0500

    Update exercise.py

--- assignments/06PromedioConDecision/src/exercise.py
+++ assignments/06PromedioConDecision/src/exercise.py
@@ -1,5 +1,20 @@
 def main():
     #escribe tu código abajo de esta línea
+    print("PROMEDIO")
+    cantidad = 0
+    sum = 0
+
+    while (True):
+        nota = float(input("LA NOTA ES DE: "))
+
+        if (nota == -1):
+            break
+
+        cantidad = cantidad + 1
+        sum = sum + nota
+    promedio = sum/nota
+    print("EL PROMEDIO ES DE: ", promedio)
+
     pass
 if __name__=='__main__':
     main()
