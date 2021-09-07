3c59e14535b3987a00ba83f05cc847df0eb7a00d
A01662767 <89958847+A01662767@users.noreply.github.com>
2021-09-07 13:17:02 -0500

    Update exercise.py

--- assignments/17NCuadradoMayor/src/exercise.py
+++ assignments/17NCuadradoMayor/src/exercise.py
@@ -1,8 +1,14 @@
-
-
+import math
 def main():
-    #Escribe tu código debajo de esta línea
-    pass
+    #escribe tu código abajo de esta línea
+    print("Número al cuadrado mayor que N")
+    n = int(input("NUMERO: "))
 
+    for i in [n]:
+        cuadrado = math.sqrt(i)
+        f= math.ceil(cuadrado)
+        print (f)
+
+    pass
 if __name__=='__main__':
     main()
